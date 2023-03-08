d512594514c36b2a665463d1fcd7c03629b128b5
Plvmmu10 <gluca0260@gmail.com>
2023-03-08 14:35:27 +0100

    added ticket price with data error message

--- js/script.js
+++ js/script.js
@@ -5,4 +5,45 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+// var km number
+let kmNumber = prompt("Inserisci il numero di kilometri che vuoi percorrere");
 
+// var passenger age
+let userAge = prompt("Inserisci la tua età in numeri");
+
+// const km price
+const kmPrice = 0.21;
+
+// underage discont
+
+// senior discount
+
+// var ticket price
+let ticketPrice = (kmNumber * kmPrice);
+
+// var underage ticket price
+
+
+// var senior ticket price
+
+// Ticket price appears in ticket div
+if ((! isNaN(kmNumber) && ! isNaN(userAge))) {
+    
+    document.getElementById('ticket').innerHTML=
+    `<strong>${ticketPrice}</strong>`;
+}
+// Error message if data chosen by user are not numbers
+else{
+
+    document.getElementById('ticket').innerHTML=
+    `<p class="text-danger strong">I dati inseriti non sono validi</p>`;
+
+}
+
+
+
+   
+
+
+
+    
